7d45a6c993eb5d1d58af5fa845d8d8fa856831c4
WangRuiQiao <14212335+wangruiqiao@user.noreply.gitee.com>
2025-12-05 23:35:56 +0800

    修改配置

--- src/main/java/com/example/msBackend/controller/AdminController.java
+++ src/main/java/com/example/msBackend/controller/AdminController.java
@@ -2,9 +2,11 @@ package com.example.msBackend.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @CrossOrigin
 //管理商品接口,商品的增删改查，秒杀商品的增删改查
+@RequestMapping("/ad")
 public class AdminController {
 }
